1da75a9bc3f62a58c5020162d6b0dfca70990993
Rafikul321 <92312745+Rafikul321@users.noreply.github.com>
2021-10-25 20:18:44 +0100

    Added Database table

---
+++ SoftEngSiteBrief-main/SoftEngSiteBuild/SoftEngSiteBuild/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoftEngSiteBuild
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WireframeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolsCablesOtherEquipmentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolStripMenuItem2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolStripMenuItem28_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem113_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripTextBox61_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
